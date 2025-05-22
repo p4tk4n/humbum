bad1049c37b30eab1113f89c9db9ff0d67e58626
Michael <imtheearthbender97@gmail.com>
2025-05-22 20:59:22 +0200

    foun

--- hum/phone/script/phone.gd
+++ hum/phone/script/phone.gd
@@ -1,7 +1,15 @@
 extends Control
 
-func _ready() -> void:
-	pass 
+@onready var taxi_app = $Panel/taxi_app_container
+@onready var taxi_app_button = $Panel/apps/taxi_app
 
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	taxi_app.visible = false
+
+func _on_taxi_app_pressed() -> void:
+	taxi_app.visible = true
+	taxi_app_button.visible = false
+
+func _on_home_button_app_pressed() -> void:
+	taxi_app.visible = false
+	taxi_app_button.visible = true
