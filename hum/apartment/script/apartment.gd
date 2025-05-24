e36d49a9b042b99747fb3566f5a1aa3bee26d9fc
michaljezek <jezek.michal08@gmail.com>
2025-05-24 12:34:33 +0200

    A

--- hum/apartment/script/apartment.gd
+++ hum/apartment/script/apartment.gd
@@ -1,1 +1,10 @@
-extends Node
+extends Node3D
+#svet
+
+func _ready():
+	pass
+				
+func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("ui_cancel"):
+		#_toggle_pause_menu()
+	pass
